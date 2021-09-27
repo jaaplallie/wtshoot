92e6f7d0daa67159b9eb95f08b7f352634743fb8
Jaaplallie <jaaplallie@gmail.com>
2021-09-28 01:28:44 +0200

    mockups

--- main.js
+++ main.js
@@ -1,56 +1,57 @@
 // Modules to control application life and create native browser window
-const electron = require('electron')
-const { app, Menu, BrowserWindow } = electron
-const url = require('url')
-const path = require('path')
-const isMac = process.platform === 'darwin'
+const electron                            = require('electron')
+const {app, Menu, BrowserWindow, ipcMain} = electron
+const url                                 = require('url')
+const path                                = require('path')
+const isMac                               = process.platform === 'darwin'
 
 require('update-electron-app')()
+
+let mainWindow;
 
 //Set application menu
 Menu.setApplicationMenu(null)
 //const menu = Menu.buildFromTemplate(template)
 //Menu.setApplicationMenu(menu)
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 800,
+        minHeight: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 800,
-    minHeight: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    mainWindow.setIcon(path.join(__dirname, '/src/images/logo.png'));
 
-  mainWindow.setIcon(path.join(__dirname, '/src/images/logo.png'));
+    // and load the index.html of the app.
+    mainWindow.loadURL("file:///"+__dirname+"/index.html");
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (isMac === false) app.quit()
+    if (isMac === false) app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
